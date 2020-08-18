4964f8f6f35ab679b2d7e1ddd47ebc47a550b7da
quannv <quannv.tm@gmail.com>
2020-08-18 14:10:09 +0700

    update code

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by quan nguyen on 8/18/20.
+//  Copyright © 2020 quan nguyen. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
